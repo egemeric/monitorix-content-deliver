b36ca28059c7f7170492bb44724f9a77fbcd932b
egemeric <egemeric99@gmail.com>
2022-03-22 23:19:12 +0300

    add vue.js layout

---
+++ frontend/monitor-fe/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#">Link</b-nav-item>
+          <b-nav-item href="#" disabled>Disabled</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-navbar-brand href="#">System Statistics</b-navbar-brand>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <b-container fluid class="p-3">
+      <b-row class="mb-3">
+        <b-col>Images</b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script></script>
+
+<style></style>
